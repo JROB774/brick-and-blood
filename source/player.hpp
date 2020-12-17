fabb3b764c5c150322d3c39a29bf9f919beae924
Joshua Robertson <joshrobertson774@gmail.com>
2020-12-17 23:27:27 +0000

    Added an inventory hash map to the player struct.

--- source/player.hpp
+++ source/player.hpp
@@ -5,6 +5,10 @@ GLOBAL struct Player
     float input_timer;
     bool update;
 
+    // The inventory is a lookup of different resources and the quanities of those resources.
+    // For example searching up "stone" in the inventory would return the amount of stone.
+    std::map<std::string,int> inventory;
+
 } gPlayer;
 
 INTERNAL void UpdatePlayer ();
