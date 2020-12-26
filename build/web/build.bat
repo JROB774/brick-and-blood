fcdabc445d165f473030253641d29771bcbfafc6
Joshua Robertson <joshrobertson774@gmail.com>
2020-12-26 03:07:09 +0000

    Web build now up-to-date and working.

--- build/web/build.bat
+++ build/web/build.bat
@@ -5,7 +5,7 @@ call config.bat
 
 pushd ..\..
 if not exist %OutputPath% mkdir %OutputPath%
-emcc %InputSource% %IncludeDirs% %Libraries% -std=c++17 --preload-file %AssetPath% -o %OutputName%
+emcc %InputSource% %IncludeDirs% %Libraries% -s ALLOW_MEMORY_GROWTH=1 -std=c++17 --preload-file %AssetPath% -o %OutputName%
 popd
 
 endlocal
